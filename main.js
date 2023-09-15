7fefc0962ff87d8f01012d428e58128e32c897e3
Nikolai Lønberg <niklonberg@gmail.com>
2023-09-15 16:38:35 +0200

    add js to check users color theme preference, setting a light-theme class on <html> element if so.

---
+++ main.js
@@ -0,0 +1,9 @@
+//this project will start out in default dark mode.
+
+//if user prefers light mode, we add a "light-theme" class to our
+//html element, which will change the colors used
+const prefersLight = window.matchMedia("(prefers-color-scheme:dark)").matches;
+
+if (prefersLight) {
+  document.documentElement.classList.add("light-theme");
+}
