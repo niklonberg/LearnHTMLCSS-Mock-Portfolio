dba7bbe17cbcdf0e4dd2e73abbb10099d7ee9e61
Nikolai Lønberg <niklonberg@gmail.com>
2023-09-18 10:46:57 +0200

    add event listener to toggle theme button, adding or removing light-theme class to html element

--- main.js
+++ main.js
@@ -4,6 +4,14 @@
 //html element, which will change the colors used
 const toggleTheme = document.querySelector("#toggle-theme");
 
+toggleTheme.addEventListener("click", () => {
+  if (!document.documentElement.classList.contains("light-theme")) {
+    document.documentElement.classList.add("light-theme");
+  } else {
+    document.documentElement.classList.remove("light-theme");
+  }
+});
+
 const prefersLight = window.matchMedia("(prefers-color-scheme:dark)").matches;
 
 if (prefersLight) {
